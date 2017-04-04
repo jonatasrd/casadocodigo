9f61508b96ef1ee539e9f3afc3a84ec3e2a0f469
Jonatas Ramos <jonatasrd@gmail.com>
2017-04-03 21:54:30 -0300

    adicionado funcionalidade p/ carrinho de compras

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.List;
 
@@ -27,6 +28,10 @@ public class Produto {
 
 	@DateTimeFormat
 	private Calendar dataLancamento;
+
+	public BigDecimal precoPara(TipoPreco tipoPreco) {
+		return precos.stream().filter(preco -> preco.getTipo().equals(tipoPreco)).findFirst().get().getValor();
+	}
 
 	public String getTitulo() {
 		return titulo;
